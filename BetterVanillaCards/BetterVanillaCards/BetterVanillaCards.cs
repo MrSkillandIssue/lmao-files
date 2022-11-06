c67b128ca4d6093632e28ff48025c2903a92988c
MrSkillandIssue <2990kenny@mail.ru>
2022-11-07 01:21:57 +0300

    BetterVanillaCards

---
+++ BetterVanillaCards/BetterVanillaCards/BetterVanillaCards.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using BetterVanillaCards.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace BetterVanillaCards
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class BetterVanillaCards : BaseUnityPlugin
+    {
+        private const string ModId = "com.pepega.rounds.BetterVanillaCards";
+        private const string ModName = "BetterVanillaCards";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "BVC";
+        public static BetterVanillaCards instance { get; private set; }
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<BetterRefresh>();
+        }
+    }
+}
---
+++ BetterVanillaCards/BetterVanillaCards/Cards/BetterRefresh.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace BetterVanillaCards.Cards
+{
+    class BetterRefresh : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+            UnityEngine.Debug.Log($"[{BetterVanillaCards.ModInitials}][Card] {GetTitle()} has been setup.");
+            statModifiers.health = .6f;
+            block.cdMultiplier = .4f;
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Edits values on player when card is selected
+            UnityEngine.Debug.Log($"[{BetterVanillaCards.ModInitials}][Card] {GetTitle()} has been added to player {player.playerID}.");
+
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Run when the card is removed from the player
+            UnityEngine.Debug.Log($"[{BetterVanillaCards.ModInitials}][Card] {GetTitle()} has been removed from player {player.playerID}.");
+
+        }
+
+        protected override string GetTitle()
+        {
+            return "Better Refresh";
+        }
+        protected override string GetDescription()
+        {
+            return "A Better Refresh";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Rare;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Health",
+                    amount = "-40%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+            new CardInfoStat()
+            {
+                positive = true,
+                stat = "Block",
+                amount = "-60%",
+                simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+            },
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+        public override string GetModName()
+        {
+            return "BetterVanillaCards.ModInitials";
+        }
+    }
+}
